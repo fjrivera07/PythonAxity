d77904ee70a9a1c9fec6a034bf8c2e5e1842594e
fjrivera07 <fjriverabravo@gmail.com>
2026-05-06 15:32:34 -0600

    Fundamental-Entorno-Herramientas

---
+++ main.py
@@ -0,0 +1,34 @@
+def suma(a, b):
+    resultado = a + b
+    print("Resultado:", resultado)
+    return resultado
+
+
+def funcion_larga():
+    x = (
+        1
+        + 2
+        + 3
+        + 4
+        + 5
+        + 6
+        + 7
+        + 8
+        + 9
+        + 10
+        + 11
+        + 12
+        + 13
+        + 14
+        + 15
+        + 16
+        + 17
+        + 18
+        + 19
+        + 20
+    )
+    return x
+
+
+if __name__ == "__main__":
+    suma(1, 2)
